e20e7fe7d08c285d1fef332542a7b8337d4b9b03
张彩嵘 <940481948@qq.com>
2020-12-19 20:38:22 +0800

    feat(uitl): add utils

---
+++ util/doc.go
@@ -0,0 +1,8 @@
+// @program:     web-template
+// @file:        doc.go
+// @author:      edte
+// @create:      2020-12-19 20:23
+// @description: 
+package util
+
+// 这个包放那些不知道怎么分类，或者比较杂的，或者有重复代码的，抽象在这个包里，或者解决循环导包问题
